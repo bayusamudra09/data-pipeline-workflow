072efd40bec3003b5db356d2ab9cdc4cb9a44e88
bayusamudra09 <60831095+bayusamudra09@users.noreply.github.com>
2020-12-18 17:52:09 +0800

    Update data_pipeline.py

--- airflow/dags/data_pipeline.py
+++ airflow/dags/data_pipeline.py
@@ -18,7 +18,7 @@ default_args = {
 	"depends_on_past": False,
 	"email_on_failure": False,
 	"email_on_retry": False,
-	"email": "bayu.samudra.009@gmail.com",
+	"email": "test.mail@gmail.com",
 	"retries": 1,
 	"retry_delay": datetime.timedelta(minutes=5)
 }
@@ -37,10 +37,10 @@ def download_data():
 	    kasus = {"tanggal": tanggal, "provinsi": provinsi, "positif": positif, "sembuh": sembuh, "meninggal": meninggal}
 	    data.append(kasus)
 
-	with open(f'/home/bayu_samudra_009/airflow/dags/files/covid_{tanggal}.json', 'w') as file:
+	with open(f'~/airflow/dags/files/covid_{tanggal}.json', 'w') as file:
 	    json.dump(data, file)
 
-with open('/home/bayu_samudra_009/airflow/dags/files/slack_token', mode='r') as text:
+with open('~/airflow/dags/files/slack_token', mode='r') as text: #read your slack token from file and declare it as a variable
 	pasw = text.readline()
 
 token = str(pasw).strip('\n')
@@ -48,6 +48,7 @@ token = str(pasw).strip('\n')
 
 with DAG(dag_id="data_pipeline", schedule_interval="@daily", default_args= default_args, catchup=False) as dag:
 	
+	#create a task to check availability of api 
 	covid_api_available = HttpSensor(
 		task_id = "api_avaliability",
 		method= "GET",
@@ -57,7 +58,8 @@ with DAG(dag_id="data_pipeline", schedule_interval="@daily", default_args= defau
 		poke_interval = 5,
 		timeout = 20
 	)
-
+	
+	#create a task to check specific file exists
 	covid_dataset_file = FileSensor(
 		task_id = "covid_file",
 		fs_conn_id = "data_path",
@@ -65,20 +67,22 @@ with DAG(dag_id="data_pipeline", schedule_interval="@daily", default_args= defau
 		poke_interval= 5,
 		timeout = 20
 	)
-
+	
+	#download data using the function we create above
 	downloading_data = PythonOperator(
 		task_id = 'downloading_data',
 		python_callable= download_data
 	)
-
+	
+	#save data we've download to hdfs using BashOperator
 	save_to_hdfs = BashOperator(
 		task_id = "save_to_hdfs",
 		bash_command = f"""
-            	hdfs dfs -copyFromLocal /home/bayu_samudra_009/airflow/dags/files/covid_{str(datetime.date.today())}.json /user/bayu/
+            	hdfs dfs -copyFromLocal ~/airflow/dags/files/covid_{str(datetime.date.today())}.json /user/bayu/
 		"""
 	)
 
-	
+	#create table to insert the after modified 
 	creating_table = HiveOperator(
         task_id="creating_table",
         hive_cli_conn_id="hive_conn",
@@ -97,12 +101,13 @@ with DAG(dag_id="data_pipeline", schedule_interval="@daily", default_args= defau
         	FIELDS TERMINATED BY ','
         	STORED AS TEXTFILE
         """
-    )
-
+    	)
+	
+	#adding more column 
 	data_clean = SparkSubmitOperator(
 		task_id = "data_cleaning",
 		conn_id = "spark_default",
-		application = "/home/bayu_samudra_009/airflow/dags/scripts/addingcolumn.py",
+		application = "~/airflow/dags/scripts/addingcolumn.py",
 		verbose = False
 	)
 
